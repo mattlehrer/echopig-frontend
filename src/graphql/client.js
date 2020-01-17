453d7626a160808ef32964e2724952d8ad60a9e9
Matt Lehrer <lehrerm@gmail.com>
2020-01-17 13:16:21 -0800

    send jwt with new authclient

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -4,3 +4,10 @@ import 'cross-fetch/polyfill';
 export const client = new ApolloClient({
   uri: 'http://localhost:3000/graphql',
 });
+
+export const AuthClient = (token) => new ApolloClient({
+  uri: 'http://localhost:3000/graphql',
+  headers: {
+    authorization: `Bearer ${token}`,
+  }
+});

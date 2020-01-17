b19937d6b5b1ff9c74ce95d8084b23ea674c4be2
Matt Lehrer <lehrerm@gmail.com>
2020-01-17 13:19:42 -0800

    add confirmation route

---
+++ src/routes/confirmation/[token].js
@@ -0,0 +1,32 @@
+import { mutate } from 'svelte-apollo';
+import { client } from '../../graphql/client';
+import { CONFIRM_EMAIL } from '../../graphql/queries';
+import { addMsg } from '../../utils/add-msg';
+
+export async function get(req, res, next) {
+  if (req.body) {
+    try {
+      const response = await mutate(client, {
+        mutation: CONFIRM_EMAIL, 
+        variables: { 
+          token: req.params.token,
+          },
+      });
+      console.log(response);
+      if (!response.errors) {
+        req.session.user = response.data.confirmEmail;
+        req.session.messages = addMsg('info', 'Thank you for confirming your email address.', req.session.messages);
+        console.log(JSON.stringify(req.session));
+        return res.redirect('/');
+      } else {
+        return res.redirect('/');
+      }
+    }
+    catch (e) {
+      res.json(e);
+    }
+		next();
+	} else {
+		next();
+	}
+}

73c9d1031e359da792bcb08bc7c6ae32d91b698e
Matt Lehrer <lehrerm@gmail.com>
2020-01-17 13:17:32 -0800

    add queries

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -15,10 +15,20 @@ export const LOGIN = gql`
   login(user: $loginUser) {
     token
     user {
-      username, email, postTag, saveTag, isVerified
+      username, email, avatar, postTag, saveTag, isVerified, facebook, twitter
     }
   }
 }
+`;
+
+export const UPDATE_USER = gql`
+  mutation UpdateUser($updateUserInput: UpdateUserInput!) {
+    updateUser(
+      fieldsToUpdate: $updateUserInput
+    ) {
+      username, email, avatar, postTag, saveTag, isVerified, facebook, twitter
+    }
+  }
 `;
 
 export const CREATE_USER = gql`
@@ -26,7 +36,27 @@ export const CREATE_USER = gql`
     createUser(
       createUserInput: $createUserInput
     ) {
-      username, email, postTag, saveTag, isVerified
+      username, email, avatar, postTag, saveTag, isVerified, facebook, twitter
+    }
+  }
+`;
+
+export const RESEND_CONFIRM_EMAIL = gql`
+  mutation ConfirmEmail($email: String!) {
+    resendConfirmEmail(
+      email: $email
+    ) {
+      username, email, avatar, postTag, saveTag, isVerified, facebook, twitter
+    }
+  }
+`;
+
+export const CONFIRM_EMAIL = gql`
+  mutation ConfirmEmail($token: String!) {
+    confirmEmail(
+      token: $token
+    ) {
+      username, email, avatar, postTag, saveTag, isVerified, facebook, twitter
     }
   }
 `;

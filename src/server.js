79247207776b9b8f2b13820ea04f1b3e103d9f75
Matt Lehrer <lehrerm@gmail.com>
2020-01-17 13:13:45 -0800

    add helmet, format, add cookie messages

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const MongoStore = require('connect-mongo')(session);
 
 require('dotenv').config();
 
-const { PORT, NODE_ENV, COOKIE_SECRET, DOMAIN, DB_URI, DB_NAME } = process.env;
+const { PORT, COOKIE_SECRET, DB_URI, DB_NAME } = process.env;
 
 mongoose.Promise = global.Promise;
 mongoose.connect(DB_URI, {
@@ -45,73 +45,79 @@ const app = express();
 //   res.locals.nonce = uuidv4();
 //   next();
 // });
-app.use(helmet({
-  frameguard: true,
-  noSniff: true,
-  featurePolicy: {
-    features: {
-      geolocation: ["'none'"],
-      midi: ["'none'"],
-      notifications: ["'none'"],
-      push: ["'none'"],
-      syncXhr: ["'none'"],
-      microphone: ["'none'"],
-      camera: ["'none'"],
-      magnetometer: ["'none'"],
-      gyroscope: ["'none'"],
-      speaker: ["'self'"],
-      vibrate: ["'none'"],
-      fullscreen: ["'none'"],
-      payment: ["'none'"],
+app.use(
+  helmet({
+    frameguard: true,
+    noSniff: true,
+    featurePolicy: {
+      features: {
+        geolocation: ["'none'"],
+        midi: ["'none'"],
+        notifications: ["'none'"],
+        push: ["'none'"],
+        syncXhr: ["'none'"],
+        microphone: ["'none'"],
+        camera: ["'none'"],
+        magnetometer: ["'none'"],
+        gyroscope: ["'none'"],
+        speaker: ["'self'"],
+        vibrate: ["'none'"],
+        fullscreen: ["'none'"],
+        payment: ["'none'"],
+      },
     },
-  },
-  contentSecurityPolicy: {
-    directives: {
-      // defaultSrc: ["'self'", 'use.fontawesome.com', 'cdn.jsdelivr.net'],
-      // styleSrc: ["'self'", 'cdn.jsdelivr.net', 'use.fontawesome.com'],
-      // scriptSrc: [
-      //   "'self'",
-      //   "use.fontawesome.com",
-      //   (req, res) => `'nonce-${res.locals.nonce}'`
-      // ],
-      mediaSrc: ['*'],
-      // fontSrc: ["'self'"],
+    contentSecurityPolicy: {
+      directives: {
+        // defaultSrc: ["'self'", 'use.fontawesome.com', 'cdn.jsdelivr.net'],
+        // styleSrc: ["'self'", 'cdn.jsdelivr.net', 'use.fontawesome.com'],
+        // scriptSrc: [
+        //   "'self'",
+        //   "use.fontawesome.com",
+        //   (req, res) => `'nonce-${res.locals.nonce}'`
+        // ],
+        mediaSrc: ['*'],
+        // fontSrc: ["'self'"],
+      },
     },
-  },
-  referrerPolicy: { policy: 'no-referrer-when-downgrade' },
-}));
-app.use(session({
-  store: new MongoStore({
-    mongooseConnection: mongoose.connection,
-    collection: 'sessions',
+    referrerPolicy: { policy: 'no-referrer-when-downgrade' },
   }),
-  cookie: {
-    // httpOnly: true,
-    // secure: true,
-    // signed: true,
-    // domain: DOMAIN,
-    maxAge: 1000 * 60 * 60 * 24 * 30, // 30 days
-  },
-  secret: COOKIE_SECRET,
-  name: 'epCookie',
-  resave: false,
-  saveUninitialized: true,
-}));
+);
+app.use(
+  session({
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      collection: 'sessions',
+    }),
+    cookie: {
+      // httpOnly: true,
+      // secure: true,
+      // signed: true,
+      // domain: DOMAIN,
+      maxAge: 1000 * 60 * 60 * 24 * 30, // 30 days
+    },
+    secret: COOKIE_SECRET,
+    name: 'epCookie',
+    resave: false,
+    saveUninitialized: true,
+  }),
+);
 app.use(express.static('static'));
 app.use(bodyParser.json());
 // app.use((req, res, next) => {
-//   console.log(req.user);
 //   console.log(req.session.user);
-//   if (typeof req.session.user === 'undefined') {
-//     req.session.user = {};
-//   }
+//   // if (typeof req.session.user === 'undefined') {
+//   //   req.session.user = {};
+//   // }
 //   next();
 // });
-app.use(sapper.middleware({
-  session: req => ({
-    user: req.session && req.session.user
-  })
-}));
+app.use(
+  sapper.middleware({
+    session: req => ({
+      user: req.session && req.session.user,
+      messages: req.session && req.session.messages,
+    }),
+  }),
+);
 app.listen(PORT, err => {
   if (err) console.log('error', err);
 });

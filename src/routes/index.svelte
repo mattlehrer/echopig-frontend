2b9ed1711c808835bb7f2ee33d623339a4680142
Matt Lehrer <lehrerm@gmail.com>
2020-01-17 13:18:02 -0800

    change landing page for logged in user

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,7 @@
 <script>
   import Register from '../components/Register.svelte';
+  import { stores } from '@sapper/app';
+  const { session } = stores();
 </script>
 
 <svelte:head>
@@ -7,18 +9,29 @@
 	<title>Echopig</title>
 </svelte:head>
 
-<div class="container">
-  <div class="columns">
-    <div class="column is-half">
-      <h1 class="is-size-1">Echopig</h1>
-      <h2 class="is-size-3">The best way to share your favorite podcast episodes.</h2>
-      <div class="is-size-5">You don't need a new app, you just need a better way to find and share the best episodes.</div>
-    </div>
-    <div class="column"></div>
-    <div id="sign-up-column" class="column is-two-fifths">
-      <div class="block">
-        <Register />
+{#if !$session.user}
+  <div class="container">
+    <div class="columns">
+      <div class="column is-half">
+        <h1 class="is-size-1">Echopig</h1>
+        <h2 class="is-size-3">The best way to share your favorite podcast episodes.</h2>
+        <div class="is-size-5">You don't need a new app, you just need a better way to find and share the best episodes.</div>
+      </div>
+      <div class="column"></div>
+      <div id="sign-up-column" class="column is-two-fifths">
+        <div class="block">
+          <Register />
+        </div>
       </div>
     </div>
   </div>
+{/if}
+
+<div class="container">
+  <h2>
+    Hot Episodes
+  </h2>
+  <h2>
+    Hot Podcasts
+  </h2>
 </div>

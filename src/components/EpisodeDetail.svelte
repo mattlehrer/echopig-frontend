66864c1a5560d6b2631b7019ac4428e2bd06a774
Matt Lehrer <lehrerm@gmail.com>
2020-01-21 11:02:14 -0800

    add episode page

---
+++ src/components/EpisodeDetail.svelte
@@ -0,0 +1,67 @@
+<script>
+  // import AudioPlayer from './AudioPlayer.svelte';
+
+  // create a new query to lookup number of posts ref-ing this episode _id
+
+  export let title, image, podcast, published, description, mp3URL;
+</script>
+
+<style>
+  h3 {
+    margin-bottom: 1em;
+  }
+
+  audio {
+    width: 65%;
+  }
+
+  /* .post-count {
+    align-self: flex-end;
+    flex-shrink: 0;
+  }
+
+  .snout {
+    line-height: 1.5rem;
+    padding-right: 0.25rem;
+  } */
+</style>
+
+<div class="columns">
+  <div class="column is-one-third">
+    <img src={image ? image : podcast.artworkUrl600} alt="{title} Logo" />
+  </div>
+
+  <div class="column metadata">
+    <h2 class="is-size-4">
+      <a
+        rel="prefetch"
+        href={'/p/' + podcast.iTunesID ? 'i' + podcast.iTunesID : podcast._id}>
+        {podcast.title}
+      </a>
+    </h2>
+    <h1 class="is-size-3">{title}</h1>
+    {#if published}
+      <h3 class="is-size-6 has-text-weight-light">{published}</h3>
+    {/if}
+    <audio
+      controls
+      src={mp3URL} type="audio/mpeg">
+        Your browser does not support the
+        <code>audio</code> element.
+    </audio>
+    <!-- <AudioPlayer {mp3URL} /> -->
+    <div class="description">
+      <p>{description}</p>
+    </div>
+  </div>
+
+  <div class="epfooter">
+
+    <!-- {#if posts}
+      <div class="post-count is-size-7">
+        <span class="snout">🐽</span>
+        {posts}
+      </div>
+    {/if} -->
+  </div>
+</div>
---
+++ src/routes/e/[id].svelte
@@ -0,0 +1,61 @@
+<script context="module">
+  import { client } from '../../graphql/client';
+  import { GET_EPISODE } from '../../graphql/queries';
+
+  export async function preload(page) {
+    const cache = await client.query({
+        query: GET_EPISODE,
+        variables: {
+          episode: page.params.id,
+        },
+      });
+    const published = cache.data.episode.releaseDate ? new Date(cache.data.episode.releaseDate).toLocaleDateString('en-us', {
+          dateStyle: 'long',
+        }) : undefined;
+    return {
+      id: page.params.id,
+      cache,
+      title: `${cache.data.episode.podcast.title} - ${cache.data.episode.title}`,
+      published,
+    };
+  }
+</script>
+
+<script>
+  import { query, restore } from 'svelte-apollo';
+  import { fade } from 'svelte/transition';
+  import Spinner from '../../components/Spinner.svelte';
+  import EpisodeDetail from '../../components/EpisodeDetail.svelte';
+
+  export let cache;
+  export let id, title, published;
+
+  restore(client, GET_EPISODE, cache.data);
+
+  const episode = query(client, { query: GET_EPISODE,
+        variables: {
+          episode: id,
+        }, });
+</script>
+
+<style>
+  .wrapper {
+    padding: 0 0.75rem;
+  }
+</style>
+
+<svelte:head>
+  <title>{title} - Echopig</title>
+</svelte:head>
+
+<div class="wrapper">
+  {#await $episode}
+    <Spinner />
+  {:then result}
+    <div in:fade|local class="episode-detail">
+      <EpisodeDetail {...result.data.episode} {published} />
+    </div>
+  {:catch error}
+    <p style="color: red">{error.message}</p>
+  {/await}
+</div>

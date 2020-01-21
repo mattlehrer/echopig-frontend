43c78efe5cca43177a9c0d00d780fb92f8e192b8
Matt Lehrer <lehrerm@gmail.com>
2020-01-21 11:03:01 -0800

    style spinner

--- src/components/Spinner.svelte
+++ src/components/Spinner.svelte
@@ -1,24 +1,16 @@
 <script>
-  export let colorRgba = "rgba(52, 148, 148, .2)";
-  export let color = "#349494";
-  export let height = "4px";
-  export let width = "40%";
+  export let colorRgba = 'rgba(52, 148, 148, .2)';
+  export let color = '#349494';
+  export let height = '4px';
+  export let width = '40%';
 
   $: styles = [
     `height: ${height}`,
     `width: ${width}`,
-    `background-color: ${colorRgba}`
-  ].join(";");
-  $: lineStyles = [
-    `height: ${height}`,
-    `background-color: ${color}`
-  ].join(";");
+    `background-color: ${colorRgba}`,
+  ].join(';');
+  $: lineStyles = [`height: ${height}`, `background-color: ${color}`].join(';');
 </script>
-
-<div style={styles} class="wrapper">
-    <div style={lineStyles} class="small-line one"/>
-    <div style={lineStyles}  class="small-line two"/>
-</div>
 
 <style>
   .wrapper {
@@ -37,12 +29,14 @@
     animation-fill-mode: forwards;
   }
   .small-line.one {
-    animation: 2.1s cubic-bezier(0.65, 0.815, 0.735, 0.395) 0s infinite normal none running long;
+    animation: 2.1s cubic-bezier(0.65, 0.815, 0.735, 0.395) 0s infinite normal
+      none running long;
   }
   .small-line.two {
-    animation: 2.1s cubic-bezier(0.165, 0.84, 0.44, 1) 1.15s infinite normal none running short;
-    }
-    @keyframes long {
+    animation: 2.1s cubic-bezier(0.165, 0.84, 0.44, 1) 1.15s infinite normal
+      none running short;
+  }
+  @keyframes long {
     0% {
       left: -35%;
       right: 100%;
@@ -70,5 +64,9 @@
       right: -8%;
     }
   }
- 
 </style>
+
+<div style={styles} class="wrapper">
+  <div style={lineStyles} class="small-line one" />
+  <div style={lineStyles} class="small-line two" />
+</div>
